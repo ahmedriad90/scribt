f77c981ec535aae58571ebda4917019ea47f3bca
ahmedriad90 <96264376+ahmedriad90@users.noreply.github.com>
2022-01-30 16:02:51 +0200

    Add files via upload

--- s.sh
+++ s.sh
@@ -1,28 +1,32 @@
 ﻿#!/bin/sh
-#
+##setup command=wget wget https://raw.githubusercontent.com/ahmedriad90/scribt/main/s.sh -O - | /bin/sh 
 
-wget -O /var/volatile/tmp/scriptahmeriad.tar.gz "https://raw.githubusercontent.com/ahmedriad90/scribt/main/scriptahmeriad.tar.gz"
+version=1.0
 
-echo ""
-# Download and install plugin
+
+
 cd /tmp
-set +e
-echo "===> Downloading And Installing  script Please Wait ......"
-echo
+rm -f *scriptahmeriad*
+# Download new version
+wget "https://raw.githubusercontent.com/ahmedriad90/scribt/main/scriptahmeriad.tar.gz"
+
+
+
+# Install new version
 tar -xzf scriptahmeriad.tar.gz -C /
-
+set +e
 rm -f scriptahmeriad.tar.gz
+cd ..
 
-
-echo ""
 sync
-echo "##############################################################"
-echo "#         script INSTALLED SUCCESSFULLY             #"
-echo "#             UPLOADED BY AHMED RIAD                #"
-echo "##############################################################"
-echo "**************************************************************"
-echo "##############################################################"
-echo "#              RESTART YOUR STB NOW                  #"
-echo "##############################################################"
-
+echo "#########################################################"
+echo "#          KeyAdder INSTALLED SUCCESSFULLY              #"
+echo "#                 Raed  &  mfaraj57                     #"              
+echo "#                     support                           #"
+echo "#   https://www.tunisia-sat.com/forums/threads/3955125/ #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+sleep 3
+killall -9 enigma2
 exit 0
