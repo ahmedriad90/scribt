c5a6f6c7045aaa52da6fee333dba3f512b37e8b1
ahmedriad90 <96264376+ahmedriad90@users.noreply.github.com>
2022-01-30 16:12:10 +0200

    Add files via upload

--- s.sh
+++ s.sh
@@ -1,29 +1,27 @@
 ﻿#!/bin/sh
-##
+#
 
+wget -O /var/volatile/tmp/media.tar.gz "https://doc-0o-0s-docs.googleusercontent.com/docs/securesc/lqlqhcrp1i4vs2hin58ud9ouedr4epp3/39lfo9nrndeoba0ikg3h481gunaog1g4/1643551800000/03860807689812204033/03860807689812204033/1I9U4_WJq2_E6zHkDOkZV2m5h1hz54R7W?e=download&authuser=0&nonce=v4oe9mb5tbg38&user=03860807689812204033&hash=bb2mtn7fl7m92tupfivn9js7hc9pqf91"
 
+echo ""
+# Download and install plugin
 cd /tmp
-rm -f scriptahmeriad.tar.gz
-# Download new version
-wget "https://doc-0o-0s-docs.googleusercontent.com/docs/securesc/lqlqhcrp1i4vs2hin58ud9ouedr4epp3/4i3e8pl5voq6vsqap2ucset2bs6snspi/1643551425000/03860807689812204033/03860807689812204033/1I9U4_WJq2_E6zHkDOkZV2m5h1hz54R7W?e=download&authuser=0&nonce=arcfuffmdo2l8&user=03860807689812204033&hash=dagqtderkdmua4hmq7suesjhql3i6q8t"
-
-
-
-# Install new version
-tar -xzf scriptahmeriad.tar.gz -C /
+set -e
+echo "===> Downloading And Installing  picons Please Wait ......"
+echo
+tar -xzf media.tar.gz -C /
 set +e
-rm -f scriptahmeriad.tar.gz
-cd ..
+rm -f media.tar.gz
 
+echo ""
 sync
-echo "#########################################################"
-echo "#          KeyAdder INSTALLED SUCCESSFULLY              #"
-echo "#                 Raed  &  mfaraj57                     #"              
-echo "#                     support                           #"
-echo "#   https://www.tunisia-sat.com/forums/threads/3955125/ #"
-echo "#########################################################"
-echo "#           your Device will RESTART Now                #"
-echo "#########################################################"
-sleep 3
-killall -9 enigma2
+echo "##############################################################"
+echo "#         PICONS INSTALLED SUCCESSFULLY             #"
+echo "#             UPLOADED BY ELIESAT                #"
+echo "##############################################################"
+echo "**************************************************************"
+echo "##############################################################"
+echo "#              RESTART YOUR STB NOW                  #"
+echo "##############################################################"
+
 exit 0
