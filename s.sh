7ea87d344a68be04addb6a47e0dd3af3b46a50ad
ahmedriad90 <96264376+ahmedriad90@users.noreply.github.com>
2022-01-30 16:24:14 +0200

    Add files via upload

--- s.sh
+++ s.sh
@@ -1,7 +1,7 @@
 ﻿#!/bin/sh
 #
 
-wget -O /var/volatile/tmp/media.tar.gz "https://doc-0o-0s-docs.googleusercontent.com/docs/securesc/lqlqhcrp1i4vs2hin58ud9ouedr4epp3/39lfo9nrndeoba0ikg3h481gunaog1g4/1643551800000/03860807689812204033/03860807689812204033/1I9U4_WJq2_E6zHkDOkZV2m5h1hz54R7W?e=download&authuser=0&nonce=v4oe9mb5tbg38&user=03860807689812204033&hash=bb2mtn7fl7m92tupfivn9js7hc9pqf91"
+wget -O /var/volatile/tmp/usr.tar.gz "https://drive.google.com/uc?export=download&id=1I9U4_WJq2_E6zHkDOkZV2m5h1hz54R7W"
 
 echo ""
 # Download and install plugin
@@ -9,9 +9,9 @@ cd /tmp
 set -e
 echo "===> Downloading And Installing  picons Please Wait ......"
 echo
-tar -xzf media.tar.gz -C /
+tar -xzf usr.tar.gz -C /
 set +e
-rm -f media.tar.gz
+rm -f usr.tar.gz
 
 echo ""
 sync
